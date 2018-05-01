543075fadc07522843196cf4f846644c92a22194
Pedro Rodrigues <pedroagrodrigues@gmail.com>
2018-05-02 00:45:01 +0100

    Moved the Screen
    Moved the screen to a new place. Created a layout for the keys, fixed some bugs with the login sequence. 
    Also added the PEPE simulator with its simulation.
    
    To do next, add system of tryes for the login.
    Make the new registration.
    Then see what's next.

--- PizzO/PizzO.asm
+++ PizzO/PizzO.asm
@@ -1,14 +1,14 @@
 ;Periféricos
-POWER                       EQU 1A0H
-Option                      EQU 1B0H
-OK                          EQU 1C0H
-USERB                       EQU 1D0H
-USERE                       EQU 1D8H
-PASSB                       EQU 1E0H
-PASSE                       EQU 1E8H
-USERSCREEN                  EQU 233H
-PASSSCREEN                  EQU 253H
-BDDATACHECH                 EQU 3000H
+POWER                       EQU 0B0H
+Option                      EQU 0C0H
+OK                          EQU 0D0H
+USERB                       EQU 0F3H
+USERE                       EQU 0FAH
+PASSB                       EQU 0113H
+PASSE                       EQU 011AH
+USERSCREEN                  EQU 53H
+PASSSCREEN                  EQU 73H
+BDDATACHECK                 EQU 3000H
 BD                          EQU 3010H
 ;---Display---
 Display                     EQU 20H
@@ -57,6 +57,16 @@ place 10H
   String "================"
 place 90H
   String "================"
+;--- Buttons---
+place 0B0H
+  String "_ <- Power ON   "
+  String "_ <- Option     "
+  String "_ <- OK         "
+  String "   Username:    "
+  String "   ________     "
+  String "   Password:    "
+  String "   ________     "
+
 ;---initial user account---
 place 3010H
 String "Pedro12312345678"
@@ -76,22 +86,29 @@ place 6000H
     MOV R0, POWER                   ;Move the priferic memory code to R0
     IS_ON:
       MOVB R1, [R0]                 ;Gets the value of the Memory
+      MOV R2, 30H
+      SUB R1, R2
       CMP R1, 1
-      JNE IS_ON                     ;Repeats untill the Priferic is set to 1 (ON)
+      JNZ IS_ON                     ;Repeats untill the Priferic is set to 1 (ON)
     ON:
       MOV R2, StartMenu             ;Sets the memory position of the screen to sho
       CALL ShowDisplay              ;Turns on the screen with starting Menu
       CALL CleanPerif               ;Cleans all the priferics
     FirstMenu:
+      MOV R0, OK
+      MOVB R0, [R0]
+      MOV R2, 30H
+      SUB R0, R2
+      CMP R0, 1
+      JNE FirstMenu
       MOV R0, Option                ;Sets the memory position for the Option priferic
       MOVB R0, [R0]                 ;Reads the input Option
-      CMP R0, 0
-      JZ FirstMenu                  ;Keeps the FirstMenu on Display untill an option is selected
+      SUB R0, R2
       CMP R0, 1                     ;Switch case for the FirstMenu options
       JZ DisplayRegistration        ;1 for Registration
       CMP R0, 2                     ;2 for Login
       JZ DisplayLogin
-
+      JMP FirstMenu                 ;Keeps the FirstMenu on Display untill an option is selected
 
 
 
@@ -120,7 +137,7 @@ place 6000H
     MOV R0, POWER
     MOV R1, Option
     MOV R2, OK
-    MOV R3, 0
+    MOV R3, 5FH
     MOVB [R0], R3
     MOVB [R1], R3
     MOVB [R2], R3
@@ -149,8 +166,8 @@ place 6000H
     PUSH R2
     PUSH R1
     PUSH R0
-    mov R0, Display
-    mov R1, Display_End
+    MOV R0, Display
+    MOV R1, Display_End
     Cycle:                          ;Places the Display char by char
       MOV R3, [R2]
       MOV [R0], R3
@@ -178,7 +195,7 @@ place 6000H
     POP R0
 
   DisplayLogin:                   ;Behaviour of the Menu Login
-    PUSH R6
+    PUSH R7
     PUSH R5
     PUSH R4
     PUSH R3
@@ -190,52 +207,57 @@ place 6000H
     CALL CleanPerif               ;Cleans all Perif
     MOV R3, OK
     LoginCycle:
-      MOV R0, USERB
-      MOV R4, USERE
-      MOV R2, USERSCREEN
-      MOV R5, BDDATACHECH
-      MOV R6, [R6]
-      GetUser:
+      MOV R0, USERB               ;Gets where the user input starts
+      MOV R4, USERE               ;Gets where the user input ends
+      MOV R2, USERSCREEN          ;Gets where in the screen is the user space
+      MOV R5, BDDATACHECK         ;Memory for a data comparation
+      ADD R4, 1                   ;Adds 1 to the password end position to make comparation easyer
+    GetUser:                      ;Loops untill has cheked all user inputs
+        MOV R7, 5FH               ;This is the ASCII code for "_"
         MOVB R1, [R0]
-        CMP R1, 0
-        JZ NEXTCHARU
+        CMP R1, R7
+        JZ NEXTCHARU              ;Jumps if he finds "_" meaning that user has not inserted nothing
         MOVB [R2], R1
         NEXTCHARU:
-        MOVB [R5], R1
+        MOVB [R5], R1             ;Copy the input of username to the DBDATACHECK
         ADD R0, 1
         ADD R2, 1
         ADD R5, 1
         CMP R0, R4
-        JNE GetUser
-      MOV R0, PASSB
-      MOV R4, PASSE
-      MOV R2, PASSSCREEN
-      MOV R8, 2AH
-      GetPass:
+        JNZ GetUser               ;Keeps the loop going untill we have all characters
+      MOV R0, PASSB               ;Gets where the password input starts
+      MOV R4, PASSE               ;Gets where the password input ends
+      ADD R4, 1                   ;Adds 1 to the password end position to make comparation easyer
+      MOV R2, PASSSCREEN          ;Gets where in the screen the password should be
+      MOV R8, 2AH                 ;Saves the '*' to be placed as password in the screen
+      GetPass:                    ;Loops untill has checked all password inputs
         MOVB R1, [R0]
-        CMP R1, 0
+        CMP R1, R7
         JZ NEXTCHARP
         MOVB [R2], R8
-        NEXTCHARP:
+        NEXTCHARP:                ;Skips the Screen '*' if there's no input on that place
         MOVB [R5], R1
         ADD R0, 1
         ADD R2, 1
         ADD R5, 1
         CMP R0, R4
-        JNE GetPass
-      MOVB R0, [R3]
+        JNZ GetPass               ;Keeps the loop going untill we have all characters
+      MOVB R0, [R3]               ;Places in R0 the value of the memory for the input of OK
+      MOV R7, 30H
+      SUB R0, R7                  ;Converts the input from a string number to an integer number
       CMP R0, 1
-      JNE LoginCycle
-    CALL BDCONSULT
+      JNE LoginCycle              ;Keeps looping untill OK is "pressed"
+    CALL BDCONSULT                ;Calls the consultation to the DataBase
     POP R0
     POP R1
     POP R2
     POP R3
     POP R4
     POP R5
-    POP R6
+    POP R7
+    RET
 
-  BDCONSULT:
+  BDCONSULT:                    ;Consults the DataBase
     PUSH R0
     PUSH R1
     PUSH R2
@@ -243,31 +265,33 @@ place 6000H
     PUSH R4
     PUSH R5
     PUSH R6
-    MOV R0, BDDATACHECH
-    MOV R1, BD
-    MOV R4, BD
+    MOV R0, BDDATACHECK       ;Gets the initial position for the string it should compare
+    MOV R1, BD                ;Gets the first position of the DataBase
+    MOV R4, BD                ;Gets the first position of the DataBase to compare afterwards
+    MOV R5, 0                 ;Places a 0 on R5 to use as a counter
+
     TRYLOGIN:
-      MOVB R2, [R1]
-      CMP R2, 0
-      JZ ENDLOGIN
-      MOVB R3, [R0]
+      MOVB R2, [R1]           ;Gets the 1st character from the DataBase to compare
+      CMP R2, 0               ;Compares with '0' if it is zero we are at the end of the DataBase
+      JZ ENDLOGIN             ;Leaves the DataBase
+      MOVB R3, [R0]           ;Gets 1st character to compare with the one from the DataBase
       CMP R2, R3
-      JNZ NEXTUSER
+      JNZ NEXTUSER            ;If the 1st not equal should jump to the NEXT USER
       ADD R0, 1
       ADD R1, 1
       ADD R5, 1
-      MOV R6, 8
+      MOV R6, 9               ;Sets the end of comparation this is here so I can use R6 again
       CMP R5, R6
-      JZ LOGGED
+      JZ LOGGED               ;If we get here it means that all 8 bytes compared mached, the login is sucessfull
       JMP TRYLOGIN
     NEXTUSER:
-      MOV R0, BDDATACHECH
-      MOV R6, 20H
-      ADD R4, R6
-      MOV R1, R4
-      JMP TRYLOGIN
+      MOV R0, BDDATACHECK     ;Resets the initial position for the comparation String
+      MOV R6, 20H             ;Saves 20 to be incremented in memory
+      ADD R4, R6              ;Increments the memory moves to the Next place in the DataBase
+      MOV R1, R4              ;Gets the new Data to compare
+      JMP TRYLOGIN            ;Jums to try login with the new Data
     LOGGED:
-      MOV R9, 1
+      MOV R9, R4               ;Places the memory position from the mached user so we can use it later on the program
     ENDLOGIN:
     POP R6
     POP R5
